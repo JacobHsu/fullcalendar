e4388b8b1a938068812e4f9c726ce1957ef0da54
jacobhsu <jacob.hsu.tw@gmail.com>
2018-05-22 23:35:15 +0800

    bower & gulp

--- README.md
+++ README.md
@@ -13,3 +13,15 @@ FullCalendar相依於jQuery與Moment.js，記得要先載入
 `npm install gulp gulp-webserver -save-dev` 
 
 `$git subtree push --prefix dist origin gh-pages`  
+
+### bower & gulp 
+
+用Bower自動下載jQuery, Bootstrap等Libery，再Gulp做打包
+bower 用來安裝包，處理依賴    
+gulp 用來管理、合併、minify、uglify 文件   
+
+`npm install -g bower`  
+`bower init` bower.json  
+`bower install <package>`  bower_components 裡面放的 是下載下來的外掛
+
+`npm install --save-dev main-bower-files`  
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
+var mainBowerFiles = require('main-bower-files');
+
+
+gulp.task('bower', function() {
+    return gulp.src(mainBowerFiles())
+               .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('vendorJs', ['bower'], function(){
+  return gulp.src(['./bower_components/jquery/dist/jquery.js'])
+			.pipe(gulp.dest('./dist/js'))
+})
 
 gulp.task('webserver', function() {
   gulp.src('./dist/')
@@ -12,4 +24,4 @@ gulp.task('webserver', function() {
     }));
 });
 
-gulp.task('default',['webserver']);
+gulp.task('default',['bower', 'vendorJs', 'webserver']);
